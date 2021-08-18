54907ee3091dae9fbdef8b0e0a91e65ff507ad65
yoles <yo-0ne@hotmail.fr>
2021-08-18 18:48:56 +0200

    Post and PostCategory models

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Post, PostCategory
 
 # Register your models here.
+
+@admin.register(PostCategory)
+class PostCategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'category',
+        'published',
+        'created_at',
+    )
+
+    list_filter = (
+        'category__name',
+        'published',
+    )
+
+    autocomplete_fields = ['category']
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+class PostCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    def slug(self):
+        return slugify(self.name)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey('PostCategory',
+                                 null=True,
+                                 blank=True,
+                                 on_delete=models.DO_NOTHING)
+    published = models.BooleanField(default=False)
+    text = RichTextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
