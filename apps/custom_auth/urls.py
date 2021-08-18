05cb3d6ec1fb4ae2fe259fa674d3c21e7a9fc4bc
yoles <yo-0ne@hotmail.fr>
2021-08-18 18:49:31 +0200

    Password Change and Done custom template

--- apps/custom_auth/urls.py
+++ apps/custom_auth/urls.py
@@ -9,10 +9,12 @@ urlpatterns = [
     path('login/', log_in, name='login'),
     path('user/logout', log_out, name='logout_user'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-
-    path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
-    path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-
+    path(
+        'password_change/', 
+        views.PasswordChangeView.as_view(template_name='login/password_change.html'),
+        name='password_change'
+    ),
+    path('password_change/done/', views.PasswordChangeDoneView.as_view(template_name='login/password_done.html'), name='password_change_done'),
     path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
