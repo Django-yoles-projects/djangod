3a3fd28629209fb0ac3d1a9bfd795cb270344c22
yoles <yo-0ne@hotmail.fr>
2021-08-19 19:01:21 +0200

    Fixed Init method Call

--- apps/login/forms.py
+++ apps/login/forms.py
@@ -7,15 +7,11 @@ class LoginForm(AuthenticationForm):
     
     username = UsernameField(label="Nom d'utilisateur", widget=forms.TextInput(attrs={'autofocus': True}))
 
-    # def __init__(self, request=None, *args, **kwargs):
     def __init__(self, *args, **kwargs):
-        # request = kwargs.pop('request')
-        # self.request = request
-        super(AuthenticationForm, self).__init__(*args, **kwargs)
+        super(LoginForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
         
-
     class Meta:
         model = User
         fields = ['username', 'password']
